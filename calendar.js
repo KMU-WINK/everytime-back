48625942bfedd96b26fd334a6a43221fad226750
dygames <dygames@naver.com>
2022-01-10 14:03:55 +0900

    hotfix : calendar

--- calendar.js
+++ calendar.js
@@ -62,14 +62,14 @@ module.exports = function (app, connection) {
                     if (err) res.status(201).json({ error: err })
                     else {
                         if (typeof(req.body.userid) === typeof('')) {
-                            connection.query(`insert into calendar_user values(${req.body.id}, '${req.body.userid}', null, 1)`, (err, results, fields) => {
+                            connection.query(`insert into calendar_user values(${req.body.id}, null, '${req.body.userid}', null, 1)`, (err, results, fields) => {
                                 if (err) res.status(201).json({ error: err })
                                 else res.status(200).json({ data: results })
                             })
                         }
                         else {
                             for (let i = 0; i < req.body.userid.length; i++) {
-                                connection.query(`insert into calendar_user values(${req.body.id}, '${req.body.userid[i]}', null, 1)`, (err, results, fields) => {
+                                connection.query(`insert into calendar_user values(${req.body.id}, null, '${req.body.userid[i]}', null, 1)`, (err, results, fields) => {
                                     if (err) res.status(201).json({ error: err })
                                     else if (i === req.body.userid.length - 1) {
                                         res.status(200).json({ data: results })
