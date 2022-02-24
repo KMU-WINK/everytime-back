a4a09739186a4fcc677f04b51ea9b60af8bcfe85
dygames <dygames@naver.com>
2022-02-24 12:40:48 +0900

    add knock notification

--- notification.js
+++ notification.js
@@ -36,5 +36,31 @@ module.exports = function (app, connection) {
         })
     })
 
+    app.get('/notification_knock', function (req, res) {
+        var id = req.query.email.split('@')[0]
+        connection.query(`select u.*, c.* from calendar_user as u join calendar as c on u.calendarid = c.id where senderid is not null and userid = '${id}' and accepted = 0;`, (err, results, fields) => {
+            if (err) res.json({ error: err })
+            else res.status(200).json({ data: results })
+        })
+    })
+
+    app.post('/accept_knock', function (req, res) {
+        var id = req.body.email.split('@')[0]
+        var tid = req.body.senderEmail.split('@')[0]
+        connection.query(`update calendar_user set accepted = 1 where senderid = '${tid}' and userid = '${id}' and calendarid = ${req.body.senderEmail}`, (err, results, fields) => {
+            if (err) res.status(201).json({ error: err })
+            else res.status(200).json({ data: results })
+        })
+    })
+
+    app.post('/decline_knock', function (req, res) {
+        var id = req.body.email.split('@')[0]
+        var tid = req.body.senderEmail.split('@')[0]
+        connection.query(`delete from calendar_user where senderid = '${tid}' and userid = '${id}' and calendarid = ${req.body.senderEmail}`, (err, results, fields) => {
+            if (err) res.status(201).json({ error: err })
+            else res.status(200).json({ data: results })
+        })
+    })
+
     return router
 }
