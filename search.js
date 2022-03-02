1863a93e8d6643d68ab1f451e28285c5e8fdb6b8
DYGames <dygames@naver.com>
2022-03-03 04:25:40 +0900

    fix minor bug

--- search.js
+++ search.js
@@ -129,7 +129,7 @@ module.exports = function (app, connection) {
 
     app.get('/searchgroup', function (req, res) {
         var id = req.query.email.split('@')[0]
-        connection.query(`select *, (select count(*) from follower where userid = '${id}' and followerid = u.id) from usergroup where id like '%${req.query.query}%' or nickname like '%${req.query.query}%'`, (err, results, fields) => {
+        connection.query(`select *, (select count(*) from usergroup_user where userid = '${id}' and usergroupid = u.id) as follow from usergroup as u where id like '%${req.query.query}%' or nickname like '%${req.query.query}%'`, (err, results, fields) => {
             if (err) res.status(201).json({ error: err })
             else res.status(200).json({ data: results })
         })
