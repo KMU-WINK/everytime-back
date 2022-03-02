52e2699eb1f581a9bf1ecff9abff3ef525962b10
DYGames <dygames@naver.com>
2022-03-02 23:49:45 +0900

    add get weekly group calendar

--- calendar.js
+++ calendar.js
@@ -15,6 +15,12 @@ module.exports = function (app, connection) {
             else res.status(200).json({ data: results })
         })
     })
+    app.get('/weeklygroup', function (req, res) {
+        connection.query(`select c.* from calendar as c left join calendar_user as u on c.id = u.calendarid join (select * from usergroup_user as uu join usergroup as gg on uu.usergroupid = gg.id where gg.id = '${req.body.groupid}') as uu where ((u.senderid is null and u.userid = uu.userid) or (u.senderid is not null and u.accepted = 0 and u.senderid = uu.userid) or (u.senderid is not null and u.accepted = 1 and (u.userid = uu.userid or u.senderid = uu.userid))) and startDate >= '${req.query.year}-${req.query.month}-${req.query.day}' and startDate < (date_add('${req.query.year}-${req.query.month}-${req.query.day}', interval 1 week))`, (err, results, fields) => {
+            if (err) res.json({ error: err })
+            else res.status(200).json({ data: results })
+        })
+    })
     app.get('/daily', function (req, res) {
         var id = req.query.email.split('@')[0]
         connection.query(`select c.* from calendar as c left join calendar_user as u on c.id = u.calendarid where ((u.senderid is null and u.userid = '${id}') or (u.senderid is not null and u.accepted = 0 and u.senderid = '${id}') or (u.senderid is not null and u.accepted = 1 and (u.userid = '${id}' or u.senderid = '${id}'))) and startDate >= '${req.query.year}-${req.query.month}-${req.query.day}' and startDate < (date_add('${req.query.year}-${req.query.month}-${req.query.day}', interval 1 day))`, (err, results, fields) => {
